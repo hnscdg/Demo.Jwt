943c382dd6ad204fd5abbdf4aa0cd5f4211bc554
hnscdg <hnscdg@DESKTOP-1QJ0HT2>
2020-03-26 10:41:54 +0800

    add policy handler class

---
+++ Demo.Jwt/AuthManagement/PolicyHandler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Demo.Jwt.AuthManagement
+{
+    public class PolicyHandler : AuthorizationHandler<PolicyRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PolicyRequirement requirement)
+        {
+            //赋予用户权限
+            var userPermissions = requirement.UserPermissions;
+
+            //从AuthorizationHandlerContext转成HttpContext，以便取出表求信息
+            var httpContext = (context.Resource as AuthorizationFilterContext).HttpContext;
+
+            //请求URL
+            var questUrl = httpContext.Request.Path.Value.ToUpperInvariant();
+
+            //是否经过验证
+            var isAuthenticated = httpContext.User.Identity.IsAuthenticated;
+            if (isAuthenticated)
+            {
+                if (userPermissions.GroupBy(g => g.Url).Any(w => w.Key.ToUpperInvariant() == questUrl )) 
+                {
+                    //用户名
+                    var userName = httpContext.User.Claims.SingleOrDefault(s => s.Type == ClaimTypes.NameIdentifier).Value;
+
+                    if (userPermissions.Any(w => w.UserName == userName && w.Url.ToUpperInvariant() == questUrl )) 
+                    {
+                        context.Succeed(requirement);
+                    } else 
+                    {
+                        //无权限跳转到拒绝页面
+                        httpContext.Response.Redirect(requirement.DeniedAction);
+                    }
+                } else 
+                {
+                    context.Succeed(requirement);
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
