8e8fce10d614fdefec4657c1affc6e339d967fec
hnscdg <hnscdg@DESKTOP-1QJ0HT2>
2020-03-26 08:44:47 +0800

    first stop of using jwt

---
+++ Demo.Jwt/Controller/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Jwt.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Demo.Jwt/Controller/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Jwt.Controller
+{
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/value1
+        [HttpGet]
+        [Route("api/value1")]
+        public ActionResult<IEnumerable<string>> Get() 
+        {
+            return new string[] { "value1", "value1" };
+        }
+
+        // GET api/value2
+        [HttpGet]
+        [Route("api/value2")]
+        [Authorize]
+        public ActionResult<IEnumerable<string>> Get2()
+        {
+            return new string[] { "value2", "value2" };
+        }
+
+
+    }
+}
